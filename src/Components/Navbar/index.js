1e2293bb653a598dbeb16b12702b10ab59618b15
Dudi1896 <denzelu181@gmail.com>
2022-04-07 13:55:28 -0400

    Navbar fixes

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -3,19 +3,18 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import LogoDude from "../../assets/images/logo_Dude.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faBars} from "@fortawesome/free-solid-svg-icons"
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
     <div className="nav-bar">
-      <Link className="logo" to="/">
-        <img src={LogoDude} alt="logo" />
-      </Link>
-      <nav>
-          <NavLink exact="true" activeclassname="active" to="/">
-            <FontAwesomeIcon className="menu-icon" icon={faBars} color="#4d4d4e" />"
-          </NavLink>
-      </nav>
+        <Link className="logo" to="/">
+          <img src={LogoDude} alt="logo" />
+        </Link>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon
+            className="menu-icon" icon={faBars} color="#4d4d4e"/>
+        </NavLink>
     </div>
   );
 };
