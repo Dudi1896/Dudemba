9df3834e9877a727ae2b3c6072ee33378bb4ca3e
Dudi1896 <denzelu181@gmail.com>
2022-08-11 18:56:38 -0400

    navbar edits

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -21,18 +21,20 @@ const Navbar = () => {
       </Link>
       <NavLink exact="true" activeclassname="active" to="/">
         <ul>
-          <FontAwesomeIcon
-            className="hamburger" icon={faBars} color="#4d4d4e" onClick={toggleHamburger} />
+          <FontAwesomeIcon className="hamburger" icon={faBars} 
+            color="#4d4d4e" onClick={toggleHamburger}> 
+            
+          </FontAwesomeIcon>
         </ul>
       </NavLink>
+      className={
+        hamburgerOpen ? "nav-menu expanded" : "nav-menu"
+      }
       <ul>
         <li>My Work</li>
         <li>My Resume</li>
         <li>Blog</li>
       </ul>
-
-
-
     </div>
   );
 };
