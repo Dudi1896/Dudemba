17db365ceed1034a51fad22bd3049ec2e3fd339e
Dudi1896 <denzelu181@gmail.com>
2022-08-15 23:58:43 -0400

    Home Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.scss";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./Components/Layout";
+import Home from "./Components/Home";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<Layout />} />
+        <Route index element={<Home />} />
       </Routes>
     </div>
   );
